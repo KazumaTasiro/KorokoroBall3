a3394b740765cce7ecff332e7502bbb1e3fd2deb
KazumaTasiro <k021g1450@g.neec.ac.jp>
2022-04-26 15:45:36 +0900

    課題

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VSTest : MonoBehaviour
+public class Goal : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ÉSÅ[ÉãÅI");
+    }
     // Start is called before the first frame update
     void Start()
     {
-     //Debug.Log("Hello Unity!");
+        
     }
 
     // Update is called once per frame
